c7cdfe534e6300e1ed3b88ef839f489ed3b01aff
Lucas Cava <lucas.cava@hotmail.com>
2024-05-13 01:14:46 -0300

    feat: add output

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "Public subnet ID"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "Private subnet ID"
+  value       = aws_subnet.private_subnet.id
+}
+
+output "rds_public_sg_id" {
+  description = "RDS public security group ID"
+  value       = aws_security_group.rds_public_sg.id
+}
